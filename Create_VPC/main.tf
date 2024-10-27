0fcf6ae7df3adbd49f2cfced37b098273b2a3fbc
madhavareddy97056 <81456216+madhavareddy97056@users.noreply.github.com>
2024-10-27 21:59:44 +0530

    Delete Create_VPC directory

--- Create_VPC/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "vpc-only-terra" {
-  tags = {
-    Name = "Terrfaorm_VPC"
-  }
-  cidr_block           = "13.0.0.0/16"
-  instance_tenancy     = "default"
-  enable_dns_hostnames = true
-}
