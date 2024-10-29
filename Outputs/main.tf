e59fc2b3067f7623212390a80b2d9c36abe9207d
madhavareddy97056 <81456216+madhavareddy97056@users.noreply.github.com>
2024-10-29 13:44:13 +0530

    Update main.tf

--- Outputs/main.tf
+++ Outputs/main.tf
@@ -9,7 +9,24 @@ resource "aws_instance" "test_output" {
     ENV  = "dev"
   }
 }
-
+/*
 output "show" {
   value = [aws_instance.test_output.id, aws_instance.test_output.public_ip, aws_instance.test_output.private_ip, aws_instance.test_output.public_dns]
+}*/
+
+output "instance_pub_ip" {
+  value = aws_instance.test_output.public_ip
+}
+
+output "instance_pvt_ip" {
+  value = aws_instance.test_output.private_ip
+}
+
+output "instance_ami" {
+  value = aws_instance.test_output.ami
+}
+
+output "instance_pub_arn" {
+  value = aws_instance.test_output.arn
+  sensitive = true
 }
